ad229534cf3cc7fcd31fa3a32c537703ebe1928d
JoseAMR0 <144081762+JoseAMR0@users.noreply.github.com>
2025-05-16 11:27:38 -0500

    Space Ti Version 0 Editar 4

---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-root',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class AppComponent {
+  title = 'Space-TI';
+}
